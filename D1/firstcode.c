b7b5fdcac9b807687c33cbbde312730425f19cf3
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2023-11-29 12:02:18 +0530

    29/11/2023

---
+++ D1/firstcode.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=10,b=3,c;
+    c=a+b;
+    printf("Sum=%d",c);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 100days_of_C
+I' m starting a 100days of code in C challenge for myself to practice and enhancing my problem solving skills.
