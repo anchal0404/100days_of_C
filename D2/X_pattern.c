70b5d2e8ce5689a7e39c540fa1ec4fda5d87817f
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2023-12-05 11:34:55 +0530

    Update of Day 2

---
+++ D2/X_pattern.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int n;
+    /*Prompting from the user*/
+    printf("Enter the number:");
+    scanf("%d",&n);
+    int i,j;
+
+    //Loops for printing the pattern.
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            //Condition for printing numeric value.
+            if((i==j) || (i+j == n+1))
+            {
+                printf("%d",j);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
